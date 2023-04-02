811832c0cf29dc00b992596e175cc3c2d6d5dd55
Bertan Sogutlu <bertansogutlu@gmail.com>
2023-04-03 01:07:33 +0300

    project migration olusturuldu

--- data/migrations/20230402204704_project.js
+++ data/migrations/20230402204704_project.js
@@ -3,7 +3,36 @@
  * @returns { Promise<void> }
  */
 exports.up = function(knex) {
-  
+  return knex.schema
+  .createTable('projects',table=>{
+    table.increments('project_id')
+    table.string('project_name').notNullable()
+    table.string('project_description')
+    table.boolean('project_completed').defaultTo(false)
+  })
+  .createTable('resources',table=>{
+    table.increments('resource_id')
+    table.string('resource_name').notNullable().unique()
+    table.string('resource_description')
+  })
+  .createTable('tasks',table=>{
+    table.increments('task_id')
+    table.string('task_description').notNullable()
+    table.string('task_notes')
+    table.boolean('task_completed').defaultTo(false)
+    table.integer('project_id').notNullable().unsigned()
+    .references('project_id').inTable('projects')
+    .onUpdate('CASCADE').onDelete('CASCADE')
+  })
+  .createTable('project_resources',table=>{
+    table.increments('project_resource_id')
+    table.integer('project_id').notNullable().unsigned()
+    .references('project_id').inTable('projects')
+    .onUpdate('CASCADE').onDelete('CASCADE')
+    table.integer('resource_id').notNullable().unsigned()
+    .references('resource_id').inTable('resources')
+    .onUpdate('CASCADE').onDelete('CASCADE')
+  })
 };
 
 /**
@@ -11,5 +40,9 @@ exports.up = function(knex) {
  * @returns { Promise<void> }
  */
 exports.down = function(knex) {
-  
-};
+    return knex.schema
+    .dropTableIfExists('projects')
+    .dropTableIfExists('resources')
+    .dropTableIfExists('tasks')
+    .dropTableIfExists('project_resources')
+  };
