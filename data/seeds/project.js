ea1840bc100d036991228e5da3cf665d8b850f1f
Bertan Sogutlu <bertansogutlu@gmail.com>
2023-04-03 01:28:58 +0300

    project seed olusturuldu

--- data/seeds/project.js
+++ data/seeds/project.js
@@ -4,10 +4,59 @@
  */
 exports.seed = async function(knex) {
   // Deletes ALL existing entries
-  await knex('table_name').del()
-  await knex('table_name').insert([
-    {id: 1, colName: 'rowValue1'},
-    {id: 2, colName: 'rowValue2'},
-    {id: 3, colName: 'rowValue3'}
-  ]);
+  const defaultProjects = [
+    {
+      project_name : 'Workintech',
+      project_description : 'On going'
+    },
+    {
+      project_name : 'Sun Express',
+      project_description : 'On flight'
+    }
+  ];
+  const defaultResources = [
+    {
+      resource_name: 'Money',
+      resource_description: 'USD'
+    },
+    {
+      resource_name: 'Food',
+      resource_description: 'KG'
+    }
+  ];
+  const defaultTasks = [
+    {
+      task_description: 'Not for everyone',
+      task_notes: 'Need some challenge',
+      project_id: 1
+    },
+    {
+      task_description: 'Killing me',
+      task_notes: 'Need help',
+      project_id: 1
+    },
+    {
+      task_description: 'Dream on',
+      task_notes: 'Why not',
+      project_id: 2
+    }
+  ];
+  const defaultProjects_Resources = [
+    {
+      project_id: 1,
+      resource_id: 1
+    },
+    {
+      project_id: 1,
+      resource_id: 2
+    },
+    {
+      project_id: 2,
+      resource_id: 2
+    },
+  ];
+  await knex('projects').insert(defaultProjects);
+  await knex('resources').insert(defaultResources);
+  await knex('tasks').insert(defaultTasks);
+  await knex('project_resources').insert(defaultProjects_Resources);
 };
