ea7969beb0e88154a5cd6ca7b2b0c4f23361131b
Bertan Sogutlu <bertansogutlu@gmail.com>
2023-04-03 02:21:14 +0300

    task router olusturuldu

--- api/task/router.js
+++ api/task/router.js
@@ -1,1 +1,13 @@
 // `/api/tasks` router buraya
+const router = require('express').Router();
+const taskModel = require('./model');
+
+router.get('/', async (req,res,next)=>{
+    try {
+        res.json('Task')
+    } catch (error) {
+        next(error)
+    }
+});
+
+module.exports = router;
