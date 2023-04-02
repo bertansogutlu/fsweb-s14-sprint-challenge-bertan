060c1acaf3f54a5c44d8cdbc9059145fe9019060
Bertan Sogutlu <bertansogutlu@gmail.com>
2023-04-03 02:06:20 +0300

    knexfile duzeltildi

--- knexfile.js
+++ knexfile.js
@@ -9,6 +9,18 @@ module.exports = {
     client: 'sqlite3',
     connection: {
       filename: './dev.sqlite3'
+    },
+    migrations: {
+      directory: './data/migrations'
+    },
+    seeds: {
+      directory: './data/seeds'
+    },
+    useNullAsDefault: true,
+    pool:{
+      afterCreate:(conn,done)=>{
+        conn.run('PRAGMA foreign_keys = ON',done)
+      }
     }
   },
 
